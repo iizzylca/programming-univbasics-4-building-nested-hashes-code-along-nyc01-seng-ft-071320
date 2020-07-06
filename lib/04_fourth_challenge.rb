52d212a48649bd66dbbfb4fa65410e5ed84ce683
Israel Canessa <israellcanessa@gmail.com>
2020-07-06 13:31:57 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -28,8 +28,8 @@ def fourth_challenge
           attitude: "confused"
         },
         {
-          name: "Nurse"
-          age: "44"
+          name: "Nurse", 
+          age: "44", 
           attitude: "worried"
         }
       ]
