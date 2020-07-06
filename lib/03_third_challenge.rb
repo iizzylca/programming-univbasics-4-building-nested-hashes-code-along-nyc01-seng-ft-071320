17160270461953a959629df3cc6db6f259787e83
Israel Canessa <israellcanessa@gmail.com>
2020-07-06 13:28:20 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -1,33 +1,17 @@
 def third_challenge
   epic_tragedy = {
-   :montague => {
-      :patriarch => {
-        name:
-        age:
-      },
-      :matriarch => {
-        name:
-        age:
-      },
-      :hero => {
-        name:
-        age:
-      },
-      :hero_friends => []},
-    :capulet => {
-      :patriarch => {
-        name:
-        age:
-      },
-      :matriarch => {
-        name:
-        age:
-      },
-      :heroine => {
-        name:
-        age:
-      },
-      :heroine_friends => []}}
-
+     :montague => {
+        :patriarch => {name: "Lord Montague", age: "53"},
+        :matriarch => {name: "Lady Montague", age: "54"},
+        :hero => {name: "Romeo", age: "15", status: "alive"},
+        :hero_friends => []
+     },
+     :capulet => {
+        :patriarch => {name: "Lord Capulet", age: "50"},
+        :matriarch => {name: "Lady Capulet", age: "51"},
+        :heroine => {name: "Juliet", age: "15", status: "alive"},
+        :heroine_friends => []
+     }
+  }
 
 end
